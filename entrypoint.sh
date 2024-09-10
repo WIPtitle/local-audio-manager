6830a45e0e41d331b7c3a2b5846af194311ba742
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-09-10 22:11:49 +0200

    fix: made audio working with pulseaudio server between host and container

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,11 @@ cd /app
 
 pip install --upgrade pip
 
-apt-get update && apt-get install -y libpq-dev git
+apt-get update && apt-get install -y libpq-dev git pulseaudio libpulse0
+
+echo "default-server = tcp:host.docker.internal:4713" > /etc/pulse/client.conf
+echo "autospawn = no" >> /etc/pulse/client.conf
+
 pip install --no-cache-dir -r requirements.txt
 
 uvicorn app.main:app --host 0.0.0.0 --port 8000
