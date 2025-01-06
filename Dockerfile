b1a19268e5d190b2b32a5ca4e4e61ba89d5b69c2
galvagnimatteo <galvagni.matteo@protonmail.com>
2025-01-06 12:02:10 +0100

    fix: local audio manager from ubuntu to debug alsa

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10
+FROM ubuntu:22.04
 
 WORKDIR /app
 COPY requirements.txt .
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,9 @@
 
 cd /app
 
-pip install --upgrade pip
+apt-get update && apt-get install -y python3-pip libpq-dev git alsa-utils mpg123 libportaudio2 libportaudiocpp0 portaudio19-dev libasound-dev libsndfile1-dev
 
-apt-get update && apt-get install -y libpq-dev git alsa-utils mpg123 libportaudio2 libportaudiocpp0 portaudio19-dev libasound-dev libsndfile1-dev
-
-pip install --no-cache-dir -r requirements.txt
+pip3 install --upgrade pip
+pip3 install --no-cache-dir -r requirements.txt
 
 uvicorn app.main:app --host 0.0.0.0 --port 8000
