db1af6b2ec875b567c6f36ef169680d3862bd85d
galvagnimatteo <galvagni.matteo@protonmail.com>
2026-02-22 11:37:26 +0100

    fix: better caching

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
-FROM ubuntu:22.04
+FROM python:3.10-slim
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    libpq-dev git curl \
+    && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
-COPY requirements.txt .
-COPY . .
 
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
 COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
 COPY waiting.mp3 /var/lib/local-audio-manager/data/waiting.mp3
 
 EXPOSE 8000
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,3 @@
 #!/bin/sh
 
-cd /app
-
-apt update && apt-get install -y curl python3-pip libpq-dev git
-
-pip3 install --upgrade pip
-pip3 install --no-cache-dir -r requirements.txt
-
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
-
-# Loop until server up to force initialization
-while true; do
-    response=$(curl --write-out "%{http_code}" --silent --output /dev/null http://0.0.0.0:8000)
-    if [ "$response" -eq 404 ]; then
-        break
-    else
-        sleep 1
-    fi
-done
-
-wait
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
